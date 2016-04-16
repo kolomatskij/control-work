6f86e6bd76d9831e3df8a17242be47c45b2a3a8a
Kirill Akatov <kee15roll@gmail.com>
2016-04-16 22:13:12 +0300

    Version 1
    It works only with one file now.
    Here are two types of users:
    1.Admin
    2.User
    
    They have different permissions.
    
    With file we have 4 actions:
    1.Create file
    2.Change file
    3.Open for read file
    4.Delete file
    
    File are created in .txt format.

---
+++ File_Sstorage/Data.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace File_Sstorage
+{
+    class Data
+    {
+        List<FileStream> Files = new List<FileStream>();
+        FileStream file;
+        public void CreateNewFile()
+        {
+                file = File.Create("1.txt");
+                Files.Add(file);
+        }
+        public void DeleteFile()
+        {
+                Files.Remove(file);
+        }
+        public void ChangeFile()
+        {
+            using (FileStream file = File.OpenWrite("1.txt"))
+
+            {
+               string value = Console.ReadLine();
+                Byte[] info =
+                  new UTF8Encoding(true).GetBytes(value);
+
+                
+                file.Write(info, 0, info.Length);
+            }
+        }
+        public void ReadFile()
+        {
+                using (FileStream file = File.OpenRead("1.txt"))
+                {
+                    byte[] b = new byte[1024];
+                    UTF8Encoding temp = new UTF8Encoding(true);
+                    while (file.Read(b, 0, b.Length) > 0)
+                    {
+                        Console.WriteLine(temp.GetString(b));
+                    }
+            }
+        }
+
+    }
+}

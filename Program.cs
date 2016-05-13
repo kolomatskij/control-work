a5cb7156e22c544cc79248633c43c9f9d0dc4489
kolomatskij <kolomatskij@gmail.com>
2016-05-13 18:03:45 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace File_Sstorage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+            user.Check();
+            Console.ReadKey();
+        }
+    }
+}
